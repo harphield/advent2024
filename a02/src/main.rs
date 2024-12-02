aa1251dd1bf28cd11433dacb952e757996cfe814
harphield <harphield@gmail.com>
2024-12-02 22:02:05 +0100

    Day 2 part 2

--- a02/src/main.rs
+++ a02/src/main.rs
@@ -5,8 +5,8 @@ use std::io::BufRead;
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
 
-    let mut safe = true;
     let mut safe_count = 0;
+
     for line_r in io::BufReader::new(file).lines() {
         match line_r {
             Ok(line) => {
@@ -14,24 +14,8 @@ fn main() -> Result<(), io::Error> {
                     .split(" ")
                     .map(|x| x.parse::<u32>().unwrap())
                     .collect::<Vec<u32>>();
-                numbers.iter().enumerate().for_each(|(i, x)| {
-                    if i > 0 {
-                        if (numbers[i - 1] == *x)
-                            || (numbers[i - 1] > *x && !(1..=3u32).contains(&(numbers[i - 1] - *x))
-                                || (numbers[i - 1] < *x
-                                    && !(1..=3u32).contains(&(*x - numbers[i - 1]))))
-                        {
-                            safe = false;
-                        }
 
-                        if i < numbers.len() - 1
-                            && ((numbers[i - 1] > *x && numbers[i + 1] > *x)
-                                || (numbers[i - 1] < *x && numbers[i + 1] < *x))
-                        {
-                            safe = false;
-                        }
-                    }
-                });
+                let safe = check_safety(&numbers, true);
 
                 if safe {
                     safe_count += 1;
@@ -39,11 +23,47 @@ fn main() -> Result<(), io::Error> {
             }
             Err(_) => break,
         }
-
-        safe = true;
     }
 
-    println!("Part 1: {}", safe_count);
+    println!("Part 2: {}", safe_count);
 
     Ok(())
+}
+
+fn check_safety(numbers: &[u32], try_to_fix: bool) -> bool {
+    let mut safe = true;
+
+    numbers.iter().enumerate().for_each(|(i, x)| {
+        if i > 0 {
+            if (numbers[i - 1] == *x)
+                || (numbers[i - 1] > *x && !(1..=3u32).contains(&(numbers[i - 1] - *x))
+                || (numbers[i - 1] < *x
+                && !(1..=3u32).contains(&(*x - numbers[i - 1]))))
+            {
+                safe = false;
+            }
+
+            if i < numbers.len() - 1
+                && ((numbers[i - 1] > *x && numbers[i + 1] > *x)
+                || (numbers[i - 1] < *x && numbers[i + 1] < *x))
+            {
+                safe = false;
+            }
+        }
+    });
+
+    if !safe && try_to_fix {
+        // try if removing one value fixes it
+        for i in 0..numbers.len() {
+            let mut new_numbers = numbers.to_vec();
+            new_numbers.remove(i);
+
+            if check_safety(&new_numbers, false) {
+                safe = true;
+                break;
+            }
+        }
+    }
+
+    safe
 }
