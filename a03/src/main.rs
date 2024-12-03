0ba636857953c2832b44c7f0b86ceb4372d235e5
harphield <harphield@gmail.com>
2024-12-03 21:54:36 +0100

    Day 3 part 2

--- a03/src/main.rs
+++ a03/src/main.rs
@@ -6,25 +6,33 @@ use std::io::BufRead;
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
 
-    let regex = Regex::new("mul\\(([0-9]+),([0-9]+)\\)").unwrap();
+    let regex = Regex::new("mul\\(([0-9]+),([0-9]+)\\)|do\\(\\)|don't\\(\\)").unwrap();
 
     let mut result = 0;
+    let mut enabled = true;
 
     for line_r in io::BufReader::new(file).lines() {
         match line_r {
             Ok(line) => {
                 let captures = regex.captures_iter(&line);
                 captures.for_each(|c| {
-                    // println!("{}", c.get(0).unwrap().as_str());
-                    result += c.get(1).unwrap().as_str().parse::<i32>().unwrap()
-                        * c.get(2).unwrap().as_str().parse::<i32>().unwrap();
+                    let method = c.get(0).unwrap().as_str();
+
+                    if method.eq("do()") {
+                        enabled = true;
+                    } else if method.eq("don't()") {
+                        enabled = false;
+                    } else if enabled {
+                        result += c.get(1).unwrap().as_str().parse::<i32>().unwrap()
+                            * c.get(2).unwrap().as_str().parse::<i32>().unwrap();
+                    }
                 })
             }
             Err(_) => break,
         }
     }
 
-    println!("Part 1: {}", result);
+    println!("Part 2: {}", result);
 
     Ok(())
 }
