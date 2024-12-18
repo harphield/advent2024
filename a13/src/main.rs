4fba9d0d1294895dfed99c5bad24b63a4d39b51e
harphield <harphield@gmail.com>
2024-12-18 23:22:36 +0100

    Day 13 part 1

--- a13/src/main.rs
+++ a13/src/main.rs
@@ -1,7 +1,7 @@
+use regex::Regex;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
-use regex::Regex;
 
 fn main() -> Result<(), io::Error> {
     let file = File::open("input.txt")?;
@@ -34,11 +34,11 @@ fn main() -> Result<(), io::Error> {
                         "A" => {
                             a_x = captures.get(2).unwrap().as_str().parse::<f32>().unwrap();
                             a_y = captures.get(3).unwrap().as_str().parse::<f32>().unwrap();
-                        },
+                        }
                         "B" => {
                             b_x = captures.get(2).unwrap().as_str().parse::<f32>().unwrap();
                             b_y = captures.get(3).unwrap().as_str().parse::<f32>().unwrap();
-                        },
+                        }
                         _ => {
                             panic!("oh no");
                         }
@@ -49,21 +49,30 @@ fn main() -> Result<(), io::Error> {
                     prize_y = captures.get(2).unwrap().as_str().parse::<f32>().unwrap();
                 } else {
                     // do things
-                    println!("A: {} {}, B: {} {}, Prize: {} {}", a_x, a_y, b_x, b_y, prize_x, prize_y);
+                    println!(
+                        "A: {} {}, B: {} {}, Prize: {} {}",
+                        a_x, a_y, b_x, b_y, prize_x, prize_y
+                    );
 
                     for b in 0..=100 {
                         let ax = (prize_x - (b as f32 * b_x)) / a_x;
                         let ay = (prize_y - (b as f32 * b_y)) / a_y;
 
-                        if ax > 0f32 && ax.floor() == ax && ax <= 100f32 && ay > 0f32 && ay.floor() == ay && ay <= 100f32 && ax == ay {
+                        if ax > 0f32
+                            && ax.floor() == ax
+                            && ax <= 100f32
+                            && ay > 0f32
+                            && ay.floor() == ay
+                            && ay <= 100f32
+                            && ax == ay
+                        {
                             println!("b: {} a: {}", b, ax);
                             found.push((ax as u32, b as u32));
                         }
                     }
 
-                    let possibilities = found.iter().map(|(a, b)| {
-                        (a * 3) + b
-                    }).collect::<Vec<u32>>();
+                    let possibilities =
+                        found.iter().map(|(a, b)| (a * 3) + b).collect::<Vec<u32>>();
 
                     if possibilities.len() > 0 {
                         min_tokens += possibilities.iter().min().unwrap();
