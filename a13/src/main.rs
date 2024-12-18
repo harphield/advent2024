b854140ca41a6fadcee39bbc590c1e3f6273406a
harphield <harphield@gmail.com>
2024-12-19 00:19:48 +0100

    Day 13 part 2

--- a13/src/main.rs
+++ a13/src/main.rs
@@ -9,17 +9,18 @@ fn main() -> Result<(), io::Error> {
     let button_regex = Regex::new("Button (.+): X\\+([0-9]+), Y\\+([0-9]+)").unwrap();
     let prize_regex = Regex::new("Prize: X=([0-9]+), Y=([0-9]+)").unwrap();
 
-    let mut a_x = 0f32;
-    let mut a_y = 0f32;
-    let mut b_x = 0f32;
-    let mut b_y = 0f32;
+    let mut a_x = 0f64;
+    let mut a_y = 0f64;
+    let mut b_x = 0f64;
+    let mut b_y = 0f64;
 
-    let mut prize_x = 0f32;
-    let mut prize_y = 0f32;
+    let mut prize_x = 0f64;
+    let mut prize_y = 0f64;
 
     let mut found = vec![];
 
     let mut min_tokens = 0;
+    let mut min_tokens_2 = 0;
 
     for line_r in io::BufReader::new(file).lines() {
         match line_r {
@@ -32,12 +33,12 @@ fn main() -> Result<(), io::Error> {
 
                     match letter {
                         "A" => {
-                            a_x = captures.get(2).unwrap().as_str().parse::<f32>().unwrap();
-                            a_y = captures.get(3).unwrap().as_str().parse::<f32>().unwrap();
+                            a_x = captures.get(2).unwrap().as_str().parse::<f64>().unwrap();
+                            a_y = captures.get(3).unwrap().as_str().parse::<f64>().unwrap();
                         }
                         "B" => {
-                            b_x = captures.get(2).unwrap().as_str().parse::<f32>().unwrap();
-                            b_y = captures.get(3).unwrap().as_str().parse::<f32>().unwrap();
+                            b_x = captures.get(2).unwrap().as_str().parse::<f64>().unwrap();
+                            b_y = captures.get(3).unwrap().as_str().parse::<f64>().unwrap();
                         }
                         _ => {
                             panic!("oh no");
@@ -45,8 +46,8 @@ fn main() -> Result<(), io::Error> {
                     }
                 } else if prize_regex.is_match(&line) {
                     let captures = prize_regex.captures(&line).unwrap();
-                    prize_x = captures.get(1).unwrap().as_str().parse::<f32>().unwrap();
-                    prize_y = captures.get(2).unwrap().as_str().parse::<f32>().unwrap();
+                    prize_x = captures.get(1).unwrap().as_str().parse::<f64>().unwrap();
+                    prize_y = captures.get(2).unwrap().as_str().parse::<f64>().unwrap();
                 } else {
                     // do things
                     println!(
@@ -55,15 +56,15 @@ fn main() -> Result<(), io::Error> {
                     );
 
                     for b in 0..=100 {
-                        let ax = (prize_x - (b as f32 * b_x)) / a_x;
-                        let ay = (prize_y - (b as f32 * b_y)) / a_y;
+                        let ax = (prize_x - (b as f64 * b_x)) / a_x;
+                        let ay = (prize_y - (b as f64 * b_y)) / a_y;
 
-                        if ax > 0f32
+                        if ax > 0f64
                             && ax.floor() == ax
-                            && ax <= 100f32
-                            && ay > 0f32
+                            && ax <= 100f64
+                            && ay > 0f64
                             && ay.floor() == ay
-                            && ay <= 100f32
+                            && ay <= 100f64
                             && ax == ay
                         {
                             println!("b: {} a: {}", b, ax);
@@ -78,6 +79,22 @@ fn main() -> Result<(), io::Error> {
                         min_tokens += possibilities.iter().min().unwrap();
                     }
 
+                    // Part 2
+                    prize_x = prize_x + 10000000000000f64;
+                    prize_y = prize_y + 10000000000000f64;
+
+                    let a_maybe = (prize_y * b_x - prize_x * b_y) / (b_x * a_y - a_x * b_y);
+                    let b_maybe_x = (prize_x - a_maybe * a_x) / b_x;
+                    let b_maybe_y = (prize_y - a_maybe * a_y) / b_y;
+
+                    if b_maybe_x == b_maybe_y
+                        && a_maybe.floor() == a_maybe
+                        && b_maybe_x.floor() == b_maybe_x
+                    {
+                        println!("a maybe: {} b maybe: {}", a_maybe, b_maybe_x);
+                        min_tokens_2 += a_maybe as u64 * 3 + b_maybe_x as u64;
+                    }
+
                     println!();
                 }
             }
@@ -86,6 +103,7 @@ fn main() -> Result<(), io::Error> {
     }
 
     println!("Part 1: {}", min_tokens);
+    println!("Part 2: {}", min_tokens_2);
 
     Ok(())
 }
